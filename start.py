6050a0afb533a7ce72550cef787472f8e278d170
Gilmar Pupo <contact@gpupo.com>
2020-05-21 14:33:11 -0300

    Add date

--- start.py
+++ start.py
@@ -4,44 +4,26 @@
 
 import os
 from newscatcher import Newscatcher, urls
-
-from io import StringIO
-from html.parser import HTMLParser
-
-class MLStripper(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.reset()
-        self.strict = False
-        self.convert_charrefs= True
-        self.text = StringIO()
-    def handle_data(self, d):
-        self.text.write(d)
-    def get_data(self):
-        return self.text.getvalue()
-
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
+from datetime import datetime
 
 def show_list(articles):
     for news in articles:
-        # print('* [{0}]({1})\n{2}\n'.format(news.title, news.link, strip_tags(news.summary)))
         print('* [{0}]({1})\n'.format(news.title, news.link))
  
 def show_news_from(domain):
-    print('## {0} \n'.format(domain))
+    print('## {0} \n\n'.format(domain))
     source = Newscatcher(website = domain)
     results = source.get_news()
     show_list(results['articles'])
 
 def main():
     print("---\nlayout: default\n---\n")
-    print('# @gpupo news | index')
-    show_news_from('news.ycombinator.com')
-    show_news_from('nytimes.com')
-    show_news_from('globo.com')
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print('# indexed at {0}\n\n'.format(dt_string))
+    domains = ['news.ycombinator.com','nytimes.com' ,'globo.com']
+    for d in domains:
+        show_news_from(d)
     
 if __name__ == '__main__':
     main()
